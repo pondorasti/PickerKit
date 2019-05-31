ca6325d2b6f92239290bc024abb63bef8926c598
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2019-05-31 10:40:56 +0300

    Example Code

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -13,31 +13,37 @@ import PickerKit
 
 /// The ViewController
 class ViewController: UIViewController {
+    // MARK: - Properties
+    private let colorPickerView = PickerView(colorEntries:
+        [UIColor.red, UIColor.blue, UIColor.purple, UIColor.orange, UIColor.green,
+         UIColor.red, UIColor.blue, UIColor.purple, UIColor.orange, UIColor.green]
+    )
 
-    // MARK: Properties
-    
-    /// The Label
-    lazy var label: UILabel = {
-        let label = UILabel()
-        label.text = "🚀\nPickerKit\nExample"
-        label.font = .systemFont(ofSize: 25, weight: .semibold)
-        label.numberOfLines = 0
-        label.lineBreakMode = .byWordWrapping
-        label.textAlignment = .center
-        return label
-    }()
-    
-    // MARK: View-Lifecycle
-    
-    /// View did load
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+
+        configureColorPickerViewLayout()
     }
-    
-    /// LoadView
-    override func loadView() {
-        self.view = self.label
+
+    // MARK: - Methods
+
+    /// Set up layout constraints
+    private func configureColorPickerViewLayout() {
+        view.addSubview(colorPickerView)
+
+        colorPickerView.translatesAutoresizingMaskIntoConstraints = false
+        colorPickerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        colorPickerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        colorPickerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        colorPickerView.heightAnchor.constraint(equalToConstant: 64).isActive = true
+    }
+
+    /// Configure your views style and properties for your own needs
+    private func configureColorPickerViewStyling() {
+        colorPickerView.backgroundColor = .white
+        view.backgroundColor = .gray
     }
 
 }
