2d5733aa2501dd4e69ebacfb84e42e3ce251e1ba
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2019-05-31 10:37:23 +0300

    PickerKit Class

--- Sources/PickerKit.swift
+++ Sources/PickerKit.swift
@@ -8,3 +8,220 @@
 
 // Include Foundation
 @_exported import Foundation
+import UIKit
+
+class PickerView: UIView {
+    // MARK: - Properties
+    private(set) lazy var entriesPickerCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+
+        let collectionView = UICollectionView(frame: CGRect(),
+                                              collectionViewLayout: flowLayout)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        collectionView.decelerationRate = UIScrollView.DecelerationRate(rawValue: 0.5)
+
+        return collectionView
+    }()
+
+    private lazy var focusRingLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+
+        layer.borderWidth = 2
+        layer.borderColor = self.colorEntries.first?.cgColor
+
+        return layer
+    }()
+
+    public var shouldFadeOutView = false
+    private lazy var fadeOutGradientLayer: CAGradientLayer = {
+        let gradientLayer = CAGradientLayer()
+
+        let backgroundColor = self.backgroundColor ?? .white
+
+        let colors: [UIColor] = [.clear, backgroundColor, backgroundColor, .clear]
+        gradientLayer.colors = colors.map { $0.cgColor }
+        gradientLayer.locations = [0.0, 0.25, 0.75, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+
+        return gradientLayer
+    }()
+
+    private var colorEntries = [UIColor]()
+    private let focusRingRadiusDelta: CGFloat = 10
+
+    private func getSelectedColorIndex(for contentOffset: CGPoint) -> Int {
+        let itemWidth = getItemWidth(for: entriesPickerCollectionView) + lineSpacing
+        let rawIndex = Int((contentOffset.x / itemWidth).rounded())
+        let index: Int
+
+        switch rawIndex {
+        case ..<0: // underflow
+            index = 0
+        case colorEntries.count...: // overflow
+            index = colorEntries.count - 1
+        default: // index in range
+            index = rawIndex
+        }
+
+        return index
+    }
+
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(entriesPickerCollectionView)
+        entriesPickerCollectionView.register(ColorCircleCollectionViewCell.self,
+                                             forCellWithReuseIdentifier: ColorCircleCollectionViewCell.identifier
+        )
+
+        entriesPickerCollectionView.dataSource = self
+        entriesPickerCollectionView.delegate = self
+    }
+
+    init(colorEntries: [UIColor]) { //todo: ask for fadeout
+        self.init()
+
+        self.colorEntries = colorEntries
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle
+    override func updateConstraints() {
+        if entriesPickerCollectionView.constraints.isEmpty {
+            entriesPickerCollectionView.translatesAutoresizingMaskIntoConstraints = false
+
+            entriesPickerCollectionView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+            entriesPickerCollectionView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+            entriesPickerCollectionView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+            entriesPickerCollectionView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        }
+
+        super.updateConstraints()
+    }
+
+    override func draw(_ rect: CGRect) {
+        configureFocusRingLayer()
+
+        fadeOutGradientLayer.frame = bounds
+        layer.mask = fadeOutGradientLayer
+    }
+
+    // MARK: - Methods
+    private func configureFocusRingLayer() {
+        let radius = (getItemWidth(for: entriesPickerCollectionView) + focusRingRadiusDelta) / 2
+
+        let x = self.bounds.midX - radius
+        let y = self.bounds.midY - radius
+        focusRingLayer.frame = CGRect(x: x, y: y,
+                                      width: radius * 2, height: radius * 2)
+        focusRingLayer.cornerRadius = radius
+
+        self.layer.addSublayer(focusRingLayer)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension PickerView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colorEntries.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = entriesPickerCollectionView.dequeueReusableCell(
+            withReuseIdentifier: ColorCircleCollectionViewCell.identifier,
+            for: indexPath
+            ) as? ColorCircleCollectionViewCell else {
+                fatalError()
+        }
+
+        cell.backgroundColor = colorEntries[indexPath.row]
+
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension PickerView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.scrollToItem(
+            at: indexPath,
+            at: .centeredHorizontally,
+            animated: true
+        )
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension PickerView: UICollectionViewDelegateFlowLayout {
+    fileprivate var lineSpacing: CGFloat {
+        return 12
+    }
+
+    fileprivate func getItemWidth(for collectionView: UICollectionView) -> CGFloat {
+        let size = collectionView.frame.height - 20
+        return size
+    }
+
+    fileprivate func getItemSize(for collectionView: UICollectionView) -> CGSize {
+        let size = getItemWidth(for: collectionView)
+        return CGSize(width: size, height: size)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return getItemSize(for: collectionView)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return lineSpacing
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let sideInsets = (collectionView.frame.width - getItemWidth(for: collectionView)) / 2
+
+        return UIEdgeInsets(top: 0, left: sideInsets, bottom: 0, right: sideInsets)
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+extension PickerView: UIScrollViewDelegate {
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let indexOfItem = CGFloat(getSelectedColorIndex(for: targetContentOffset.pointee))
+        let itemWidth = getItemSize(for: entriesPickerCollectionView).width + lineSpacing
+
+        targetContentOffset.pointee = CGPoint(
+            x: itemWidth * indexOfItem,
+            y: targetContentOffset.pointee.y
+        )
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let selectedColorIndex = getSelectedColorIndex(for: scrollView.contentOffset)
+        focusRingLayer.borderColor = colorEntries[selectedColorIndex].cgColor
+    }
+}
+
+// MARK: - ColorCircleCollectionViewCell
+class ColorCircleCollectionViewCell: UICollectionViewCell {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        layer.cornerRadius = frame.height / 2
+    }
+}
+
+extension ColorCircleCollectionViewCell: IdentifiableCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
+protocol IdentifiableCell {
+    static var identifier: String { get }
+}
